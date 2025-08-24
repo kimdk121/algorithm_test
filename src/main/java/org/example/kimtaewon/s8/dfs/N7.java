8432c78df4098ad044d9577b00799d675121887e
kimdk121 <kimdk121@naver.com>
2025-08-24 21:15:47 +0900

    .

--- src/main/java/org/example/kimtaewon/s8/dfs/N7.java
+++ src/main/java/org/example/kimtaewon/s8/dfs/N7.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 // 조합수
 public class N7 {
 
+    static int[][] dy = new int[35][35];
+
     public static int DFS(int n, int r) {
+        if (dy[n][r] > 0) return dy[n][r];
         if (n == r || r == 0) return 1;
-        else return DFS(n-1, r-1)+DFS(n-1, r);
+        else return dy[n][r] = DFS(n-1, r-1)+DFS(n-1, r);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/kimtaewon/s8/dfs/N9.java
@@ -0,0 +1,10 @@
+package org.example.kimtaewon.s8.dfs;
+
+// 조합 구하기
+public class N9 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
